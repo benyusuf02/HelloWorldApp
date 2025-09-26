dd351a1435360c07c6e1a63b6d520bc890da27b6
benyusuf02 <ysf.ckrl.02@outlook.com>
2025-09-26 21:18:38 +0300

    hello ivy

---
+++ Apps/HelloApp.cs
@@ -0,0 +1,20 @@
+﻿namespace Helloworld.Apps;
+
+[App(icon: Icons.PartyPopper, title: "Hello")]
+public class HelloApp : ViewBase
+{
+    public override object? Build()
+    {
+        var nameState = this.UseState<string>("");
+
+        // UI
+        return Layout.Center()
+               | new Card(
+                   Layout.Vertical().Gap(6).Padding(3)
+                   | Text.H2("Hello " + (string.IsNullOrEmpty(nameState.Value) ? "there" : nameState.Value) + "!")
+                   | nameState.ToInput(placeholder: "What is your name? :))")
+                 ).Width(Size.Units(200).Max(400));
+                 
+    }
+
+    }
